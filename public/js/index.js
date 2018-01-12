b6fde286ddb730363b94e3e336ae97927aca1aa8
aviedb <aviedbachmid.ab@gmail.com>
2018-01-12 14:29:43 +0800

    Add form and show incoming msg in browser UI

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('New Message from server:', message)
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    jQuery('#messages').append(li)
 })
 
-socket.emit('createMessage', {
-    from: 'avied',
-    text: 'Hi!'
-}, function(res) {
-    console.log(res)
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault()
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    })
 })
