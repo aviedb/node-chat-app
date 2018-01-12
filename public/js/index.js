dc0b0870053fd00c52fb13e9f53b9426ada6fd29
aviedb <aviedbachmid.ab@gmail.com>
2018-01-12 18:37:36 +0800

    Sdd css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ socket.on('newMessage', function(message) {
 
 socket.on('newLocationMessage', function(message) {
     var li = jQuery('<li></li>')
-    var a = jQuery('<a target="_blank">Open Location</a>')
+    var a = jQuery('<a target="_blank">Open location</a>')
 
     li.text(`${message.from}: `)
     a.attr('href', message.locationURL)
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault()
 
+    messageTextBox = jQuery('[name=message]')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('')
     })
 })
 
@@ -43,12 +45,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Share location')
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
-    });
+    })
   }, function () {
-    alert('Unable to fetch location.');
-  });
-});
+    locationButton.removeAttr('disabled').text('Share location')
+    alert('Unable to fetch location.')
+  })
+})
