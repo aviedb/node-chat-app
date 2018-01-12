6334ab103d78841adad6846204ebfb2e6964fb83
aviedb <aviedbachmid.ab@gmail.com>
2018-01-12 10:23:43 +0800

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
+    socket.emit('newMessage', {
+        from: 'someone@filkom.com',
+        text: 'you sucks!',
+        createdAt: 121233
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('Create Message from client:', message)
+    })
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     })
